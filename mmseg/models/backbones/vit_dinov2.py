8f362eface70a78468372d0ebb7eb98c9c217550
franchesoni <marchesoniacland@gmail.com>
2024-05-28 18:29:59 +0200

    add reg model

--- mmseg/models/backbones/vit_dinov2.py
+++ mmseg/models/backbones/vit_dinov2.py
@@ -5,7 +5,7 @@ from mmengine.model import BaseModule
 @MODELS.register_module()
 class DinoVisionTransformer(BaseModule):
     def __init__(self, backbone_name, init_cfg=None, **kwargs):
-        assert backbone_name in ['dinov2_vits14', 'dinov2_vitb14']
+        assert backbone_name in ['dinov2_vits14', 'dinov2_vitb14', 'dinov2_vitb14_reg']
         super().__init__(init_cfg=init_cfg)
         self.dino = torch.hub.load('facebookresearch/dinov2', backbone_name)
 
