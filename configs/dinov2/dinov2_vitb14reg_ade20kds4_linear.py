8af0d6c38c84614a49a4ecbc3cd67a15fd3fae1d
franchesoni <marchesoniacland@gmail.com>
2024-06-01 12:45:22 +0200

    more config files for dinov2vitbreg

---
+++ configs/dinov2/dinov2_vitb14reg_ade20kds4_linear.py
@@ -0,0 +1,15 @@
+_base_ = [
+    "dinov2_vitb14reg_ade20kds1_linear.py",
+]
+
+BATCH_SIZE = 16
+NUM_WORKERS = 42
+data_root = 'data/ade/ADEChallengeData2016_ds_4'
+train_dataloader = dict(batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, dataset=dict(data_root=data_root))
+val_dataloader = dict(batch_size=1, num_workers=NUM_WORKERS, dataset=dict(data_root=data_root))
+test_dataloader = val_dataloader
+
+custom_hooks = [
+    dict(type="EarlyStoppingHook", monitor="mIoU", strict=True, patience=3)
+]
+
