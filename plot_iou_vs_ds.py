60a51cf7068cbc1aa55fb6d347040d29557973fd
franchesoni <marchesoniacland@gmail.com>
2024-09-19 10:10:50 +0200

    update plotting

--- plot_iou_vs_ds.py
+++ plot_iou_vs_ds.py
@@ -21,18 +21,34 @@ def main(work_dirs_dir='work_dirs'):
         if len(val_lines):
             entries[work_dir] = max(val_lines)
 
+    # increase font size
+    plt.rc('font', size=18)
     plt.figure()
+    name_map = {"knet": "K-Net",
+                "dinov2_vits14_": "DINOv2-small",
+                "dinov2_vitb14_": "DINOv2-base",
+                "dinov2_vitb14reg": "DINOv2reg-base"}
 
-    for model_name in ['knet', 'dinov2_vits14_', 'dinov2_vitb14_', 'dinov2_vitb14reg']:
+    for model_idx, model_name in enumerate(['knet', 'dinov2_vits14_', 'dinov2_vitb14_', 'dinov2_vitb14reg']):
         model_entries = {k: v for k, v in entries.items() if model_name in k}
         ds_entries = {(int(k.split('ds')[1].split('-')[0].split('_')[0]) if 'ds' in k else 1): v for k, v in model_entries.items()}
-        sorted_entries = dict(sorted(ds_entries.items(), key=lambda item: item[0], reverse=True))
-        plt.plot(np.log2(list(sorted_entries.keys())), sorted_entries.values(), '-o', label=model_name)
+        sorted_entries = dict(sorted(ds_entries.items(), key=lambda item: item[0], reverse=False))
+        # plt.plot(np.log2(list(sorted_entries.keys())), sorted_entries.values(), '-o', label=name_map[model_name])
+        if model_name == 'dinov2_vitb14_':
+            # remove last entry
+            sorted_entries.pop(1)
+        linestyle = ["-", "--", ":", "-."][model_idx]
+        plt.plot(20210 / np.array(list(sorted_entries.keys())), sorted_entries.values(), linestyle, label=name_map[model_name], color='k')
+        plt.plot(20210 / np.array(list(sorted_entries.keys())), sorted_entries.values(), '.', color='k')
 
-    plt.ylabel('mIoU')
-    plt.xlabel('$\log_2$(dataset downsampling factor)')
+    plt.ylabel('ADE20k val mIoU')
+    # plt.xlabel('$\log_2$(dataset downsampling factor)')
+    plt.xscale('log')
+    plt.grid()
+    plt.xlabel('Number of annotated images')#$\log_2$(dataset downsampling factor)')
     plt.legend()
-    plt.savefig('ious.png')
+    plt.tight_layout()
+    plt.savefig('ious2.png')
 
 if __name__ == '__main__':
     main()
