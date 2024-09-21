9346875acc798bb901dcc598e9b340d80e535005
franchesoni <marchesoniacland@gmail.com>
2024-09-21 23:02:31 +0200

    correct

--- configs/dinov2/dinov2_vitb14reg_ade20k_hitl_linear.py
+++ configs/dinov2/dinov2_vitb14reg_ade20k_hitl_linear.py
@@ -25,12 +25,10 @@ train_dataloader = dict(batch_size=1, dataset=dict(data_root=data_root), num_wor
 
 # create clean train dataloader
 dataset_type = 'ADE20KDataset'
-test_pipeline = [
+traintest_pipeline = [  # similar to test pipeline in that it has no augmentations but tsimilar to train pipeline in that targets are resized
     dict(type='LoadImageFromFile'),
-    dict(type='Resize', scale=(2048, 512), keep_ratio=True),
-    # add loading annotation after ``Resize`` because ground truth
-    # does not need to do resize data transform
     dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='Resize', scale=(512, 512), keep_ratio=True),  # min and max sizes
     dict(type='PackSegInputs')
 ]
 test_dataloader = dict(
@@ -43,4 +41,4 @@ test_dataloader = dict(
         data_root=data_root,
         data_prefix=dict(
             img_path='images/training', seg_map_path='annotations/training'),
-        pipeline=test_pipeline))
+        pipeline=traintest_pipeline))
